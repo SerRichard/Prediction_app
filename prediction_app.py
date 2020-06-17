154546f5c28f378756f130dd70a3e54515560c8d
SerRichard <63349506+SerRichard@users.noreply.github.com>
2020-06-17 13:31:21 +0100

    Add files via upload

--- prediction_app.py
+++ prediction_app.py
@@ -1,3 +1,12 @@
+#!/usr/bin/env python3
+
+# -- code --
+# @author Sean Hoyal
+#
+#
+#
+#
+
 from PyQt5.QtWidgets import QPushButton, QMainWindow, QVBoxLayout, QApplication, QPushButton, QHBoxLayout
 from PyQt5 import QtCore, QtWidgets, QtGui
 from PyQt5.QtGui import QIcon
@@ -15,7 +24,6 @@ class athlete(object):
 		self.history = None
 		self.sex = None
 		self.equipment = None
-		self.coeff = None
 		self.check1 = None
 		self.check2 = None
 		self.score = None
@@ -23,92 +31,9 @@ class athlete(object):
 		self.lift2 = None
 		self.result = None
 
-
-	# Merge the three lift settings
-	def predictSquat(self):
-			self.lift = 'Squat'
-			self.check1 = 'Bench'
-			self.check2 = 'Deadlift'
-			print(self.lift)
-			return(self.lift, self.check1, self.check2)
-
-	def predictBench(self):
-			self.lift = 'Bench'
-			self.check1 = 'Squat'
-			self.check2 = 'Deadlift'
-			print(self.lift)
-			return(self.lift, self.check1, self.check2)
-
-	def predictDeadlift(self):
-			self.lift = 'Deadlift'
-			self.check1 = 'Squat'
-			self.check2 = 'Bench'
-			print(self.lift)
-			return(self.lift, self.check1, self.check2)
-
-	# Merge the three history settings
-	def setWilks(self):
-			self.history = 'Wilks'
-			print(self.history)
-			return(self.history)
-	
-	def setMcCulloch(self):
-			self.history = 'McCulloch'
-			self.coeff = True
-			print(self.history)
-			return(self.history, self.coeff)
-
-	def setLifts(self):
-			self.history = 'Lifts'
-			self.coeff = False
-			print(self.history)
-			return(self.history, self.coeff)
-	
-	# Merge the three click settings
-	def setMale(self):
-		self.sex = 0
-		print(self.sex)
-		return(self.sex)
-
-	def setFemale(self):
-		self.sex = 1
-		print(self.sex)
-		return(self.sex)
-
-	def setTrans(self):
-		self.sex = 2
-		print(self.sex)
-		return(self.sex)
-
-	# Merge equipment settings
-	def setRaw(self):
-		self.equipment = 0
-		print(self.equipment)
-		return(self.equipment)
-
-	def setWraps(self):
-		self.equipment = 1
-		print(self.equipment)
-		return(self.equipment)
-
-	def setStraps(self):
-		self.equipment = 4
-		print(self.equipment)
-		return(self.equipment)
-
-	def setSPly(self):
-		self.equipment = 3
-		print(self.equipment)
-		return(self.equipment)
-
-	def setMPly(self):
-		self.equipment = 2
-		print(self.equipment)
-		return(self.equipment)
-
 class UIHome(object):
 	def setupUI(self, MainWindow):
-		MainWindow.setWindowIcon(QtGui.QIcon("squat.png"))
+		MainWindow.setWindowIcon(QtGui.QIcon("/home/seanrh/Documents/Work/Personal code/Predict lifts/Predict_app/App/squat.png"))
 		MainWindow.setGeometry(300, 300, 500, 500)
 		MainWindow.setFixedSize(500, 500)
 		MainWindow.setWindowTitle("prediction_app")
@@ -127,21 +52,24 @@ class UIHome(object):
 		self.btn_lyt = QHBoxLayout()
 		
 		self.btn_sqt = QtWidgets.QPushButton(self.btn_lbl)
-		self.btn_sqt.setIcon(QIcon('squat.png'))
+		self.btn_sqt.setObjectName('squat')
+		self.btn_sqt.setIcon(QIcon('/home/seanrh/Documents/Work/Personal code/Predict lifts/Predict_app/App/squat.png'))
 		self.btn_sqt.setIconSize(QtCore.QSize(90, 90))
 		self.btn_sqt.setFixedSize(120, 77)
 		self.btn_sqt.setStyleSheet("background-color: darkGray")
 		self.btn_lyt.addWidget(self.btn_sqt)
 
 		self.btn_bench = QtWidgets.QPushButton(self.btn_lbl)
-		self.btn_bench.setIcon(QIcon('bench.png'))
+		self.btn_bench.setObjectName('bench')
+		self.btn_bench.setIcon(QIcon('/home/seanrh/Documents/Work/Personal code/Predict lifts/Predict_app/App/bench.png'))
 		self.btn_bench.setIconSize(QtCore.QSize(90, 90))
 		self.btn_bench.setFixedSize(120, 77)
 		self.btn_bench.setStyleSheet("background-color: darkGray")
 		self.btn_lyt.addWidget(self.btn_bench)
 
 		self.btn_dead = QtWidgets.QPushButton(self.btn_lbl)
-		self.btn_dead.setIcon(QIcon('deadlift.png'))
+		self.btn_dead.setObjectName('deadlift')
+		self.btn_dead.setIcon(QIcon('/home/seanrh/Documents/Work/Personal code/Predict lifts/Predict_app/App/deadlift.png'))
 		self.btn_dead.setIconSize(QtCore.QSize(80, 80))
 		self.btn_dead.setFixedSize(120, 77)
 		self.btn_dead.setStyleSheet("background-color: darkGray")
@@ -187,21 +115,24 @@ class UIHome(object):
 		self.sex_layout = QHBoxLayout()
 		
 		self.btn_male = QtWidgets.QPushButton(self.sex_lbl)
-		self.btn_male.setIcon(QIcon('male.png'))
+		self.btn_male.setObjectName('male')
+		self.btn_male.setIcon(QIcon('/home/seanrh/Documents/Work/Personal code/Predict lifts/Predict_app/App/male.png'))
 		self.btn_male.setIconSize(QtCore.QSize(40, 40))
 		self.btn_male.setFixedSize(120, 77)
 		self.btn_male.setStyleSheet("background-color: darkGray")
 		self.sex_layout.addWidget(self.btn_male)
 		
 		self.btn_fmale = QtWidgets.QPushButton(self.sex_lbl)
-		self.btn_fmale.setIcon(QIcon('female.png'))
+		self.btn_fmale.setObjectName('female')
+		self.btn_fmale.setIcon(QIcon('/home/seanrh/Documents/Work/Personal code/Predict lifts/Predict_app/App/female.png'))
 		self.btn_fmale.setFixedSize(120, 77)
 		self.btn_fmale.setIconSize(QtCore.QSize(50, 50))
 		self.btn_fmale.setStyleSheet("background-color: darkGray")
 		self.sex_layout.addWidget(self.btn_fmale)
 		
 		self.btn_trans = QtWidgets.QPushButton(self.sex_lbl)
-		self.btn_trans.setIcon(QIcon('trans.png'))
+		self.btn_trans.setObjectName('trans')
+		self.btn_trans.setIcon(QIcon('/home/seanrh/Documents/Work/Personal code/Predict lifts/Predict_app/App/trans.png'))
 		self.btn_trans.setFixedSize(120, 77)
 		self.btn_trans.setIconSize(QtCore.QSize(60, 60))
 		self.btn_trans.setStyleSheet("background-color: darkGray")
@@ -252,7 +183,6 @@ class UIHome(object):
 
 class UISecond(object):
 	def setupUI(self, MainWindow, athlete):
-		MainWindow.setGeometry(300, 300, 500, 500)
 		MainWindow.setFixedSize(500, 500)
 		MainWindow.setWindowTitle("prediction_app")
 
@@ -429,23 +359,23 @@ class MainWindow(QMainWindow):
 
 	def startUIHome(self):
 		self.uiHome.setupUI(self)
-		self.uiHome.btn_sqt.clicked.connect(self.athlete.predictSquat)
-		self.uiHome.btn_bench.clicked.connect(self.athlete.predictBench)		
-		self.uiHome.btn_dead.clicked.connect(self.athlete.predictDeadlift)
+		self.uiHome.btn_sqt.clicked.connect(self.lft_clk)
+		self.uiHome.btn_bench.clicked.connect(self.lft_clk)		
+		self.uiHome.btn_dead.clicked.connect(self.lft_clk)
 
-		self.uiHome.btn_male.clicked.connect(self.athlete.setMale)
-		self.uiHome.btn_fmale.clicked.connect(self.athlete.setFemale)
-		self.uiHome.btn_trans.clicked.connect(self.athlete.setTrans)
+		self.uiHome.btn_male.clicked.connect(self.sex_clk)
+		self.uiHome.btn_fmale.clicked.connect(self.sex_clk)
+		self.uiHome.btn_trans.clicked.connect(self.sex_clk)
 		
-		self.uiHome.btn_wlk.clicked.connect(self.athlete.setWilks)
-		self.uiHome.btn_mcc.clicked.connect(self.athlete.setMcCulloch)
-		self.uiHome.btn_lft.clicked.connect(self.athlete.setLifts)
+		self.uiHome.btn_wlk.clicked.connect(self.hst_clk)
+		self.uiHome.btn_mcc.clicked.connect(self.hst_clk)
+		self.uiHome.btn_lft.clicked.connect(self.hst_clk)
 
-		self.uiHome.btn_raw.clicked.connect(self.athlete.setRaw)
-		self.uiHome.btn_wraps.clicked.connect(self.athlete.setWraps)
-		self.uiHome.btn_straps.clicked.connect(self.athlete.setStraps)
-		self.uiHome.btn_sply.clicked.connect(self.athlete.setSPly)
-		self.uiHome.btn_mply.clicked.connect(self.athlete.setMPly)		
+		self.uiHome.btn_raw.clicked.connect(self.equ_clk)
+		self.uiHome.btn_wraps.clicked.connect(self.equ_clk)
+		self.uiHome.btn_straps.clicked.connect(self.equ_clk)
+		self.uiHome.btn_sply.clicked.connect(self.equ_clk)
+		self.uiHome.btn_mply.clicked.connect(self.equ_clk)		
 
 		self.uiHome.btn_next.clicked.connect(self.startUISecond)
 		self.show()
@@ -467,11 +397,95 @@ class MainWindow(QMainWindow):
 		self.uiSecond.res_btn.clicked.connect(self.startUIHome)
 		self.show()
 
+	def lft_clk(self):
+		sender = self.sender()
+		if sender.objectName() == 'squat':
+			self.athlete.lift = 'Squat'
+			self.athlete.check1 = 'Bench'
+			self.athlete.check2 = 'Deadlift'
+			print(self.athlete.lift)
+			return(self.athlete.lift, self.athlete.check1, self.athlete.check2)
+	
+		if sender.objectName() == 'bench':
+			self.athlete.lift = 'Bench'
+			self.athlete.check1 = 'Squat'
+			self.athlete.check2 = 'Deadlift'
+			print(self.athlete.lift)
+			return(self.athlete.lift, self.athlete.check1, self.athlete.check2)
+		
+		if sender.objectName() == 'deadlift':
+			self.athlete.lift = 'Deadlift'
+			self.athlete.check1 = 'Squat'
+			self.athlete.check2 = 'Bench'
+			print(self.athlete.lift)
+			return(self.athlete.lift, self.athlete.check1, self.athlete.check2)
+
+	def sex_clk(self):
+		sender = self.sender()
+		if sender.objectName() == 'male':
+			self.athlete.sex = 0
+			print(self.athlete.sex)
+			return(self.athlete.sex)
+	
+		if sender.objectName() == 'female':
+			self.athlete.sex = 1
+			print(self.athlete.sex)
+			return(self.athlete.sex)
+	
+		if sender.objectName() == 'trans':
+			self.athlete.sex = 2
+			print(self.athlete.sex)
+			return(self.athlete.sex)
+	
+	def equ_clk(self):
+		sender = self.sender()
+		if sender.text() == 'Raw':
+			self.athlete.equipment = 0
+			print(self.athlete.equipment)
+			return(self.athlete.equipment)
+		
+		if sender.text() == 'Wraps':
+			self.athlete.equipment = 1
+			print(self.athlete.equipment)
+			return(self.athlete.equipment)
+		
+		if sender.text() == 'Straps':
+			self.athlete.equipment = 4
+			print(self.athlete.equipment)
+			return(self.athlete.equipment)
+
+		if sender.text() == 'Single-Ply':
+			self.athlete.equipment = 3
+			print(self.athlete.equipment)
+			return(self.athlete.equipment)
+
+		if sender.text() == 'Multi-Ply':
+			self.athlete.equipment = 2
+			print(self.athlete.equipment)
+			return(self.athlete.equipment)
+
+	def hst_clk(self):
+		sender = self.sender()
+		if sender.text() == 'Wilks':
+			self.athlete.history = 'Wilks'
+			print(self.athlete.history)
+			return(self.athlete.history)
+
+		if sender.text() == 'McCulloch':
+			self.athlete.history = 'McCulloch'
+			print(self.athlete.history)
+			return(self.athlete.history)
+
+		if sender.text() == 'Lifts':
+			self.athlete.history = 'Lifts'
+			print(self.athlete.history)
+			return(self.athlete.history)
+
 	def btn_clk(self):
 		sender = self.sender()
 		if sender.text() == "Enter":
-			self.athlete.age = int(self.uiHome.txt_age.text())
-			self.athlete.weight = int(self.uiHome.le_weight.text())
+			self.athlete.age = int(self.uiSecond.txt_age.text())
+			self.athlete.weight = int(self.uiSecond.le_weight.text())
 			print(self.athlete.age)
 			print(self.athlete.weight)
 			return(self.athlete.age, self.athlete.weight)
@@ -506,7 +520,7 @@ class MainWindow(QMainWindow):
 			y.append(self.athlete.lift1)
 			y.append(self.athlete.lift2)
 
-		model = pickle.load(open('{}_Model_{}.sav'.format(self.athlete.lift, tier), 'rb'))
+		model = pickle.load(open('/home/seanrh/Documents/Work/Personal code/Predict lifts/Predict_app/App/{}_Model_{}.sav'.format(self.athlete.lift, tier), 'rb'))
 
 		result = str(model.predict([y]))
 		result = result.replace("[", "")
